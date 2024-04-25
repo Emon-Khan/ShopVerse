e97ec8faca8480d4ec09b273649aa50e199581c3
Emon-Khan <rkemon94@gmail.com>
2024-04-25 21:06:43 +0600

    Change settings for admin panel, also add the configuration for adding  media

--- shopverse/settings.py
+++ shopverse/settings.py
@@ -123,6 +123,11 @@ STATICFILES_DIRS = [
     'shopverse/static',
 ]
 
+# media files configuration
+MEDIA_URL = 'media/'
+MEDIA_URL_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
